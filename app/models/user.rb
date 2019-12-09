097cb1cdf8f9fe5d9b5314afd8840602dff9cfc0
reina.kurogi <kuro.reian.8@gmail.com>
2019-12-09 12:54:59 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,33 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+=begin
+  def followings
+    User.joins(:relationships).where('relationships.user_id = ?', current_user.id)
+  end
+
+  def followers
+    User.joins(:relationships).where('relationships.follow_id = ?',  current_user.id)
+  end
+=end
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
